2a94abf151c5ee324a91e293ea4bfc659b1ff15c
limZyuan <58379837+limZyuan@users.noreply.github.com>
2021-05-13 20:22:53 +0800

    drag css issue

--- client/src/pages/Dashboard/components/WorkItems/workItems.js
+++ client/src/pages/Dashboard/components/WorkItems/workItems.js
@@ -12,7 +12,71 @@ import { DragDropContext } from "react-beautiful-dnd";
 export default function WorkItems() {
   const { initialData, setInitialData } = useContext(dashContext);
 
-  const onDragEnd = (result) => {};
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const start = initialData.columns[source.droppableId];
+    const finish = initialData.columns[destination.droppableId];
+
+    const newTaskIds = Array.from(start.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    if (start === finish) {
+      const newColumn = {
+        ...start,
+        taskIds: newTaskIds,
+      };
+
+      const newState = {
+        ...initialData,
+        columns: {
+          ...initialData.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+
+      setInitialData(newState);
+      return;
+    }
+
+    // Moving from one list to another
+    const startTaskIds = Array.from(start.taskIds);
+    startTaskIds.splice(source.index, 1);
+    const newStart = {
+      ...start,
+      taskIds: startTaskIds,
+    };
+
+    const finishTaskIds = Array.from(finish.taskIds);
+    finishTaskIds.splice(destination.index, 0, draggableId);
+    const newFinish = {
+      ...finish,
+      taskIds: finishTaskIds,
+    };
+
+    const newState = {
+      ...initialData,
+      columns: {
+        ...initialData.columns,
+        [newStart.id]: newStart,
+        [newFinish.id]: newFinish,
+      },
+    };
+
+    setInitialData(newState);
+  };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
